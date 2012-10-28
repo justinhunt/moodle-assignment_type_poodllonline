130a092e46f7efb4c2fd9b0650a555802b15bbf7
Thom W. Rawson <thomw@sun.ac.jp>
2012-10-28 22:17:06 +0900

    latest changes added from the mercurial repository

--- version.php
+++ version.php
@@ -22,9 +22,9 @@
  * @copyright  2012 Justin Hunt
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-$plugin->version   = 2012100100;
+$plugin->version   = 2012102100;
 $plugin->requires  = 2012062500;
 $plugin->component = 'assignment_poodllonline'; 
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '2.3.7 (Build 2012100100)';
-$plugin->dependencies = array('filter_poodll' => 2012100100);
+$plugin->release   = '2.3.8 (Build 2012102100)';
+$plugin->dependencies = array('filter_poodll' => 2012102100);
